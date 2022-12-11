34bee164cae9862431918b6315f97f2eaf305f55
svkote <kotekofly@gmail.com>
2022-12-11 16:22:22 +0300

    error

--- app.py
+++ app.py
@@ -5,5 +5,16 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 app.register_blueprint(main_blueprint)
 
+
+@app.errorhandler(404)
+def route_not_found(error):
+    return f"Такой страницы нет {error}", 404
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return f"На сервере произошла ошибка {error}", 500
+
+
 if __name__ == '__main__':
     app.run(debug=True)
